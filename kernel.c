4e588e59465a292ca0ce8086a133d39e33ab4997
PixelGoose98 <155216249+PixelGoose98@users.noreply.github.com>
2025-12-19 12:29:09 +0100

    Add Interrupt Descriptor Table structure and array

--- kernel.c
+++ kernel.c
@@ -6,6 +6,19 @@
 #define VGA_TEXT ((volatile uint16_t*)0xB8000)
 #define VGA_COLS 80
 #define VGA_ROWS 25
+#define ARRAY_IDT_LEN 255
+
+struct IntDesc64 {
+   uint16_t offset_1;        // offset bits 0..15
+   uint16_t selector;        // a code segment selector in GDT or LDT
+   uint8_t  ist;             // bits 0..2 holds Interrupt Stack Table offset, rest of bits zero.
+   uint8_t  type_attributes; // gate type, dpl, and p fields
+   uint16_t offset_2;        // offset bits 16..31
+   uint32_t offset_3;        // offset bits 32..63
+   uint32_t zero;            // reserved
+};
+
+IntDesc64 array_int_desc_64[ARRAY_IDT_LEN];
 
 static int cursor_x = 0;
 static int cursor_y = 0;
